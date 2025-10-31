f85b7078a0344b084639e0112a9a681896ef86cd
DLucia15 <dluciaroyon@gmail.com>
2025-10-31 18:18:13 +0100

    3er commit

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::resource('movies', MovieController::class);
